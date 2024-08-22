96aa8a9486fc24a0d68ab9653ab63518d9e33566
SAKSHAM SINGH <sakshamsingh4848.com>
2024-08-22 20:29:26 +0530

    Changes made to testimony

--- src/constants/index.js
+++ src/constants/index.js
@@ -173,28 +173,28 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      "Saksham is a great team worker and a valuable asset to the team.",
+    name: "Sejal",
+    designation: "Student",
+    company: "KIIT",
+    image: "https://www.rd.com/wp-content/uploads/2018/02/33_Adorable-Puppy-Pictures-that-Will-Make-You-Melt_696648019_michaelheim.jpg?fit=700%2C467",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+      "I've never met a web developer who truly cares about the team like Saksham does.",
+    name: "Jasmin",
+    designation: "Student",
+    company: "KIIT",
+    image: "https://t4.ftcdn.net/jpg/07/65/15/75/240_F_765157555_muSlnN0OtEeo2lfIm2MmE8rYB3pU26G4.jpg",
   },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
+  // {
+  //   // testimonial:
+  //   //   "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+  //   // name: "Lisa Wang",
+  //   // designation: "CTO",
+  //   // company: "456 Enterprises",
+  //   // image: "https://randomuser.me/api/portraits/women/6.jpg",
+  // },
 ];
 
 const projects = [
@@ -217,7 +217,7 @@ const projects = [
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/sakshamXcode/Swadishtha",
   },
   {
     name: "World-Wise",
@@ -259,7 +259,7 @@ const projects = [
       },
     ],
     image: reactquiz,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/sakshamXcode/React-Quiz",
   },
 ];
 
