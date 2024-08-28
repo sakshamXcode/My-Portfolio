a2fd90ff6e174abf891db04459d250a5189f32aa
SAKSHAM SINGH <sakshamsingh4848@gmail.com>
2024-08-29 02:19:27 +0530

    Changed files again

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
+
+
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      // Exclude specific modules from the build
+      external: ['some-module', 'another-module'],
   plugins: [react()],
-})
+}
+}});
