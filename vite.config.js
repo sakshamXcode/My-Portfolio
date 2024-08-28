157a087d842722975e9b2967a99b556c8dbb9b17
SAKSHAM SINGH <sakshamsingh4848@gmail.com>
2024-08-29 02:22:55 +0530

    Changed files again

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      // Exclude specific modules from the build
-      external: ['some-module', 'another-module'],
   plugins: [react()],
-}
-}});
+})
